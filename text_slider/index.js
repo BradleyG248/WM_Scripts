03bdb0a3c4119a9231716edadf55dc5d00a42064
BradleyG248 <calvinandhobbs28@gmail.com>
2020-06-25 10:32:02 -0600

    Some loading issues ironed out.

--- text_slider/index.js
+++ text_slider/index.js
@@ -18,7 +18,7 @@ function rotateText(i) {
   rotates[i].children[activeIndexes[i]].style.whiteSpace = "nowrap";
   if (rotates[i].children[activeIndexes[i]].scrollWidth > parents[i].clientWidth) {
     rotates[i].children[activeIndexes[i]].style.whiteSpace = "normal";
-    rotates[i].style.width = rotates[i].children[activeIndexes[i]].scrollWidth + "px";
+    rotates[i].style.width = parents[i].clientWidth + "px";
     rotates[i].style.height = rotates[i].children[activeIndexes[i]].scrollHeight + "px"
   }
   else {
@@ -33,24 +33,28 @@ function rotateText(i) {
 
 for (let i = 0; i < rotates.length; i++) {
   activeIndexes[i] = 0;
-  rotates[i].style.width = 0 + "px";
+  rotates[i].style.width = 50 + "vw";
+  rotates[i].style.height = 10 + "vh";
   let parent = rotates[i];
-  parent.children[0].className = "active-rot"
-  parent.children[1].className = "next-rot"
+  parent.children[0].className = "active-rot";
+  parent.children[1].className = "next-rot";
   rotates[i].style.width = parents[i].clientWidth + "px";
-  rotates[i].children[activeIndexes[i]].style.whiteSpace = "nowrap";
   if (rotates[i].children[0].scrollWidth > parents[i].clientWidth) {
-    rotates[i].children[activeIndexes[i]].style.whiteSpace = "normal";
-    rotates[i].style.width = parents[i].clientWidth + "px";
-    rotates[i].style.height = rotates[i].children[0].scrollHeight + "px";
+    rotates[i].children[0].style.whiteSpace = "normal";
+    setTimeout(function () {
+      rotates[i].style.width = parents[i].clientWidth + "px";
+      rotates[i].style.height = rotates[i].children[0].scrollHeight + "px";
+    }, 200);
   }
   else {
-    rotates[i].children[activeIndexes[i]].style.whiteSpace = "nowrap";
-    rotates[i].style.height = rotates[i].children[0].scrollHeight + "px";
-    rotates[i].style.width = rotates[i].children[0].scrollWidth + "px";
+    rotates[i].children[0].style.whiteSpace = "nowrap";
+    setTimeout(function () {
+      rotates[i].style.height = rotates[i].children[0].scrollHeight + "px";
+      rotates[i].style.width = rotates[i].children[0].scrollWidth + "px";
+    }, 200);
   }
   for (let b = 2; b < parent.children.length; b++) {
-    parent.children[b].className = "last-rot"
+    parent.children[b].className = "last-rot";
   }
-  let timer = setInterval(function () { rotateText(i) }, 2000)
+  let timer = setInterval(function () { rotateText(i) }, 2000);
 }
--- text_slider/readme.md
+++ text_slider/readme.md
@@ -1,4 +1,6 @@
 The text slide will populate inside of whatever tag it is placed in, however it was designed to populate inside text specific tags, such as <h2>, for example.
 Add a class of rotate-site to the tag the slider will be in.
 Place a span tag where text should slide, and give it a class of rotate-parent.
-After creating the span tag, place additional span tags for each line of text to slide, populating these tags with the text needed. No classes are needed for these.
+After creating the span tag, place additional span tags for each line of text to slide, populating these tags with the text needed. No classes are necessary for these.
+To use the script, place the JS file in the server, where you see fit.
+On the Default.aspx file, place the <script src="/filepath"></script> tag inside the <body> tag, at the last line just before the </body> closing tag.
